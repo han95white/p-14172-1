4cf2f0bc5001abb15b2a47baf1278cd18bda0c98
DoubleSang <hdsa1564@gmail.com>
2026-05-20 12:05:44 +0900

    011

--- src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -5,6 +5,7 @@ import com.back.domain.post.post.service.PostService;
 import com.back.domain.post.postComment.dto.PostCommentDto;
 import com.back.domain.post.postComment.entity.PostComment;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,9 +38,21 @@ public class ApiV1PostCommentController {
             @PathVariable int id
     ) {
         Post post = postService.findById(postId).get();
-
         PostComment postComment = post.findCommentById(id).get();
 
         return new PostCommentDto(postComment);
+    }
+
+    @GetMapping("/{id}/delete")
+    public String delete(
+            @PathVariable int postId,
+            @PathVariable int id
+    ) {
+        Post post = postService.findById(postId).get();
+        PostComment postComment = post.findCommentById(id).get();
+
+        postService.deleteComment(post, postComment);
+
+        return "%d번 댓글이 삭제되었습니다.".formatted(id);
     }
 }
