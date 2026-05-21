c38799f85299dc8d424c593812900566be08c9e6
DoubleSang <hdsa1564@gmail.com>
2026-05-21 10:33:18 +0900

    021

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -3,14 +3,10 @@ package com.back.domain.post.post.controller;
 import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
-import com.back.global.rsData.ForPostRsData;
 import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,7 +35,7 @@ public class ApiV1PostController {
         return new PostDto(post);
     }
 
-    @GetMapping("/{id}/delete")
+    @DeleteMapping("/{id}")
     @Transactional
     public RsData<Void> delete(@PathVariable int id) {
         Post post = postService.findById(id).get();
@@ -51,4 +47,5 @@ public class ApiV1PostController {
                 "%d번 글이 삭제되었습니다.".formatted(id)
         );
     }
+
 }
